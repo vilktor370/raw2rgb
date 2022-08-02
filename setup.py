2a0946d7dd8cc34f8599e85019ff253b2c7c5362
Haochen <haochyu@umich.edu>
2022-08-02 01:29:53 -0400

    add more build tools

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     name='raw2rgb',
     version='0.1.0',
     description='A python tool to convert .tiff image file to .png file',
-    packages=['raw2rgb'],
     author='Haochen Yu',
     zip_safe=False
 )
