eab10a971fe925fca35b059ae9abbef56e884923
Haochen <haochyu@umich.edu>
2022-08-21 21:39:46 -0400

    Add back __init__.py

---
+++ raw2rgb/__init__.py
@@ -0,0 +1,1 @@
+from raw2rgb.convert import RawConvert
--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
-
 setup(
     name='raw2rgb',
-    version='0.1.0',
-    description='A python tool to convert .tiff image file to .png file',
+    version='0.2.0',
+    description='A python tool to convert .tiff image file to .png file. GitHub link: https://github.com/vilktor370/raw2rgb',
     author='Haochen Yu',
-    zip_safe=False
+    zip_safe=False,
+    install_requires=[
+        'Pillow>=9.2.0',
+        'scipy>=1.9.0',
+        'numpy>=1.14.5',
+        'matplotlib>=3.5.2'        
+    ],
 )
